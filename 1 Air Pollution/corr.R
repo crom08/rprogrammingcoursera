6b42757db462ec9eb669454a7d7f8b1703cf7c36
Cromwell Ellamil <cromwell.ellamil.ext@nsn.com>
2014-09-08 20:04:13 +0800

    Updated corr.R

--- 1 Air Pollution/corr.R
+++ 1 Air Pollution/corr.R
@@ -9,18 +9,20 @@ corr <- function(directory, threshold = 0) {
     
     ## Return a numeric vector of correlations
     
-    correlations <- vector(mode="logical", length=0)
-    
-    testdataframe <- data.frame();
+    correlations <- vector(mode="numeric", length=0)
     monitorFiles  <- list.files(path=directory, full.names=TRUE)
     
     for (monitor in monitorFiles) {
         currentData <- read.csv(monitor)
-        print(monitor)
-        
-        testdataframe <- rbind(testdataframe, currentData[is.na(currentData$), ])
+        completeSulfate <- currentData[!is.na(currentData$sulfate), ]
+        completePollutants <- completeSulfate[!is.na(completeSulfate$nitrate), ]
+        if (nrow(completePollutants) <= threshold) {
+            next
+        }
+        correlations <- c(correlations, round(cor(completePollutants$sulfate, 
+                                                  completePollutants$nitrate), 
+                                              digits=5))
     }
-    
-    nrow(testdataframe)
-    
+    options(digits = 4)
+    correlations
 }
