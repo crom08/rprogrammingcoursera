1dcf19d9cf635cb8faf51475ebde256ac633252d
Cromwell Ellamil <cromwell.ellamil.ext@nsn.com>
2014-09-09 20:05:42 +0800

    Updated corr.R, due to incorrect output upon submission to Coursera.

--- 1 Air Pollution/corr.R
+++ 1 Air Pollution/corr.R
@@ -1,18 +1,16 @@
 corr <- function(directory, threshold = 0) {    
     correlations <- vector(mode = "numeric", length = 0)
-    monitorFiles  <- list.files(path=directory, full.names = TRUE)
+    monitorFiles <- list.files(path = directory, full.names = TRUE)
     
     for (monitor in monitorFiles) {
         currentData <- read.csv(monitor)
-        completeSulfate <- currentData[!is.na(currentData$sulfate), ]
-        completePollutants <- completeSulfate[!is.na(completeSulfate$nitrate), ]
+        completeCases <- complete.cases(currentData)
+        completePollutants <- currentData[completeCases, ]
         if (nrow(completePollutants) <= threshold) {
             next
         }
-        correlations <- c(correlations, round(cor(completePollutants$sulfate, 
-                                                  completePollutants$nitrate), 
-                                              digits = 5))
+        correlations <- append(correlations, cor(completePollutants$sulfate,
+                                                 completePollutants$nitrate))
     }
-    options(digits = 4)
     correlations
 }
